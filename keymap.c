e0beb8612cee94029fe7ddbdfd8e1f97da5072a5
12Me21 <12Me21.mc@gmail.com>
2021-06-13 13:12:03 -0400

    more keymap stuff

--- keymap.c
+++ keymap.c
@@ -9,28 +9,30 @@
 // these are mostly to reduce memory usage + make this file shorter
 // many keys use similar sequences with different numbers/terminator chars
 
+#define FORMAT(a,b,c) .output=(a),.mode=(b),.arg=(c)
+
 // ESC [ 27 ; <modifiers> ; <number> ~
 #define ESC "\x1B"
 const char* const AUTO_SEQ = ESC"[27;%d;%d~";
-#define GENERAL(n) AUTO_SEQ, F_MODS_ARG, (n)
+#define GENERAL(n) FORMAT(AUTO_SEQ, F_MODS_ARG, (n))
 // ESC O <char>
 const char* const SS3_c_FMT = ESC"O%c";
-#define SS3_c(c) SS3_c_FMT, F_ARG_MODS, (c)
+#define SS3_c(c) FORMAT(SS3_c_FMT, F_ARG_MODS, (c))
 // ESC [ 1 ; <modifiers> <char>
 const char* const CSI_1_M_c_FMT = ESC"[1;%d%c";
-#define CSI_1_M_c(c) CSI_1_M_c_FMT, F_MODS_ARG, (c)
+#define CSI_1_M_c(c) FORMAT(CSI_1_M_c_FMT, F_MODS_ARG, (c))
 // ESC [ <modifiers> <char>
 const char* const CSI_M_c_FMT = ESC"[%d%c";
-#define CSI_M_c(c) CSI_M_c_FMT, F_MODS_ARG, (c)
+#define CSI_M_c(c) FORMAT(CSI_M_c_FMT, F_MODS_ARG, (c))
 // ESC [ <number> ; <modifier> ~
 const char* const CSI_n_M_T_FMT = ESC"[%d;%d~";
-#define CSI_n_M_T(x) CSI_n_M_T_FMT, F_ARG_MODS, (x)
+#define CSI_n_M_T(x) FORMAT(CSI_n_M_T_FMT, F_ARG_MODS, (x))
 // ESC [ <number> ~
 const char* const CSI_n_T_FMT = ESC"[%d~";
-#define CSI_n_T(x) CSI_n_T_FMT, F_ARG_MODS, (x)
+#define CSI_n_T(x) FORMAT(CSI_n_T_FMT, F_ARG_MODS, (x))
 // ESC [ <char>
 const char* const CSI_c_FMT = ESC"[%c";
-#define CSI_c(x) CSI_c_FMT, F_ARG_MODS, (x)
+#define CSI_c(x) FORMAT(CSI_c_FMT, F_ARG_MODS, (x))
 
 // modifier masks
 
@@ -81,15 +83,25 @@ static KeyMap KEY_MAP_ARRAY[] = {
 	{XK_Home, 0, SS3_c('H'), .app_cursor=1},
 	{XK_Home, 0, CSI_c('H')},
 	{XK_Home, ANY, CSI_1_M_c('H')},
-	
 	{XK_End, 0, SS3_c('F'), .app_cursor=1},
 	{XK_End, 0, CSI_c('F')},
 	{XK_End, ANY, CSI_1_M_c('F')},
 	
+	// pageup/pagedown
+	{XK_Prior, 0, CSI_n_T(5)},
+	{XK_Prior, ANY, CSI_n_M_T(5)},
+	{XK_Next, 0, CSI_n_T(6)},
+	{XK_Next, ANY, CSI_n_M_T(6)},
+	
+	// Function Keys
 	{XK_F1, 0, SS3_c('P')},
 	{XK_F2, 0, SS3_c('Q')},
 	{XK_F3, 0, SS3_c('R')},
 	{XK_F4, 0, SS3_c('S')},
+	{XK_F1, ANY, CSI_1_M_c('P')},
+	{XK_F2, ANY, CSI_1_M_c('Q')},
+	{XK_F3, ANY, CSI_1_M_c('R')},
+	{XK_F4, ANY, CSI_1_M_c('S')},
 	// yes it does go  15, 17,18,19,20,21, 23,24
 	{XK_F5, 0, CSI_n_T(15)},
 	{XK_F6, 0, CSI_n_T(17)},
@@ -99,11 +111,6 @@ static KeyMap KEY_MAP_ARRAY[] = {
 	{XK_F10, 0, CSI_n_T(21)},
 	{XK_F11, 0, CSI_n_T(23)},
 	{XK_F12, 0, CSI_n_T(24)},
-	
-	{XK_F1, ANY, CSI_1_M_c('P')},
-	{XK_F2, ANY, CSI_1_M_c('Q')},
-	{XK_F3, ANY, CSI_1_M_c('R')},
-	{XK_F4, ANY, CSI_1_M_c('S')},
 	{XK_F5, ANY, CSI_n_M_T(15)},
 	{XK_F6, ANY, CSI_n_M_T(17)},
 	{XK_F7, ANY, CSI_n_M_T(18)},
@@ -113,21 +120,23 @@ static KeyMap KEY_MAP_ARRAY[] = {
 	{XK_F11, ANY, CSI_n_M_T(23)},
 	{XK_F12, ANY, CSI_n_M_T(24)},
 	
-	
-	
-	// xterm and others allow detection of ctrl+<char> for a few extra chars, using a standard format for responses:
+	// xterm and others allow detection of ctrl+(other mods)+<char> for a few extra chars, using a standard format for responses:
 	// ESC [ 26 ; <modifier mask+1> ; <key code> ~
 	{XK_period, C_ANY, GENERAL('.')},
+	{XK_comma, C_ANY, GENERAL(',')},
+	// some more that aren't usually supported:
 	{XK_grave, C_ANY, GENERAL('`')},
-	{XK_asciitilde, C_ANY, GENERAL('`')}, //wait why are we assuming these are on the same key
+	{XK_asciitilde, C_ANY, GENERAL('~')},
 	
 	// I extend this to make ctrl+i different from tab, and ctrl+m different from enter
 	// you will likely need to configure your editor to accept these
 	{XK_i, C_ANY, GENERAL('i')},
 	{XK_m, C_ANY, GENERAL('m')},
 	
+	// commands:
 	{'V', C|S, .mode=F_FUNCTION, .func=clippaste},
 	
+	// end
 	{0},
 };
 
